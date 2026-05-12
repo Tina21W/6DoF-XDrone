37597dbcaedc9f29a7e7e5c214d114eeb4ba10e4
Tina21W <t.e.l.woltersdorf@student.tudelft.nl>
2026-05-12 12:53:09 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -7,14 +7,14 @@ close all; clear; clc;
 
 %% ------------- INITIALIZATION -------------------------------------------
 % Select vehicle configuration
-vehicle_config = @original_xzylo;  % Put the name of the body_file after @
+vehicle_config = @x_Drone;  % Put the name of the body_file after @
 
 % Load configuration
 % ============================================
 [sim.prop, sim.aero, sim.initial] = vehicle_config(); %properties, aerodynamics, initial conditions
 % =============================================
 
-x0 = [sim.initial.v0 sim.initial.omega0 sim.initial.quat0 sim.initial.pos0];
+x0 = [sim.initial.v0 sim.initial.omega0 sim.initial.quat0 sim.initial.pos0 sim.initial.u_error_int0];
 sim.initial.x0 = x0;
 
 % --- Simulation setup ---
@@ -38,11 +38,33 @@ fprintf('----------------------------------\n')
 
 %% ------------- CONTROL OPTIONS -----------------------------------------
 sim.options.control.control_law = @OAP;    % choose @OAP or @SPL
-sim.options.control.waypoint = [-50; 120; -60]
+
+%% ------------- TRAJECTORY DEFINITION -----------------------------------
+% 12-point figure-8: two tangent circles, ordered for smooth entry.
+R  = 100;                    % circle radius [m]
+z0 = sim.initial.pos0(3);   % altitude [m]
+
+sim.options.control.waypoints = [
+    R*cosd(315),  R + R*sind(315),  z0
+    R*cosd(  0),  R + R*sind(  0),  z0
+    R*cosd( 45),  R + R*sind( 45),  z0
+    R*cosd( 90),  R + R*sind( 90),  z0
+    R*cosd(135),  R + R*sind(135),  z0
+    R*cosd(180),  R + R*sind(180),  z0
+    R*cosd(135), -R + R*sind(135),  z0
+    R*cosd(180), -R + R*sind(180),  z0
+    R*cosd(225), -R + R*sind(225),  z0
+    R*cosd(270), -R + R*sind(270),  z0
+    R*cosd(315), -R + R*sind(315),  z0
+    R*cosd(  0), -R + R*sind(  0),  z0
+]';
+
+sim.options.control.waypoint_radius = 20.0;
+sim.options.control.loop = true;
 
 %% ------------- PLOTTING OPTIONS -----------------------------------------
-sim.options.propeller_on = false;
-sim.options.mBlades_on = false;
+sim.options.propeller_on = true;
+sim.options.mBlades_on = true;
 
 sim.options.body_plotting = true;
 sim.options.non_rotating_plotting = true;
@@ -55,6 +77,7 @@ options_integration = options_premaker(sim.options.live_plotting_calculation, si
 
 fprintf('Starting integration...\n'); tic; 
 % Start the integration process using the ODE solver
+clear controller;
 [t, x] = ode45(@(t,x) sixDoF_wrapper(t,x,sim), tspan, x0, options_integration);
 
 time_ode = toc; 
